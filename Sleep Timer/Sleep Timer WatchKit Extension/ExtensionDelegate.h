c8e847e4be272d5939acfc6d4d09ed21386ecce5
Artem Russkikh <rusartx@gmail.com>
2017-07-06 02:32:41 +0500

    Add WatchKit application target

---
+++ Sleep Timer/Sleep Timer WatchKit Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  Sleep Timer WatchKit Extension
+//
+//  Created by Artem Russkikh on 06.07.17.
+//  Copyright © 2017 Artem Russkikh. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ Sleep Timer/Sleep Timer WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  Sleep Timer WatchKit Extension
+//
+//  Created by Artem Russkikh on 06.07.17.
+//  Copyright © 2017 Artem Russkikh. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
